84853f06d9cbee2bad6b1849ebb74bd4a3e9dc5e
Ethan Sznajder <ethan.sznajder@epitech.eu>
2023-07-10 20:40:46 +0200

    add: mobile view

---
+++ src/routes/workitLandingPage/components/socialProof/index.tsx
@@ -0,0 +1,31 @@
+import "./styles.css";
+
+interface Props {
+  number: number;
+  title: string;
+  description: string;
+}
+
+export const SocialProof = ({ number, title, description }: Props) => {
+  return (
+    <div className="socialProofWrapper">
+      <div
+        className="socialProofNumber"
+        style={{
+          fontVariationSettings: "'opsz' 144, 'wght' 600, 'SOFT' 0",
+        }}
+      >
+        {number}
+      </div>
+      <div
+        className="socialProofTitle"
+        style={{
+          fontVariationSettings: "'opsz' 144, 'wght' 600, 'SOFT' 0",
+        }}
+      >
+        {title}
+      </div>
+      <div className="socialProofDescription">{description}</div>
+    </div>
+  );
+};
--- src/routes/workitLandingPage/index.tsx
+++ src/routes/workitLandingPage/index.tsx
@@ -1,7 +1,107 @@
+import LogoLight from "../../assets/workitLandingPage/images/logo-light.svg";
+import LogoDark from "../../assets/workitLandingPage/images/logo-dark.svg";
+import FacebookLogo from "../../assets/workitLandingPage/images/icon-facebook.svg";
+import TwitterLogo from "../../assets/workitLandingPage/images/icon-twitter.svg";
+import InstagramLogo from "../../assets/workitLandingPage/images/icon-instagram.svg";
+import BackgroundPurple from "../../assets/workitLandingPage/images/bg-purple.svg";
+import BackgroundGhostWhite from "../../assets/workitLandingPage/images/bg-ghost-white.svg";
+import Iphone from "../../assets/workitLandingPage/images/image-hero.webp";
+import Founder from "../../assets/workitLandingPage/images/image-founder.webp";
+import { SocialProof } from "./components/socialProof";
+import "./styles.css";
+
+const socialProofData = [
+  {
+    number: 1,
+    title: "Actionable insights",
+    description:
+      "Optimize your products, improve customer satisfaction and stay ahead of the competition with our product analytics.",
+  },
+  {
+    number: 2,
+    title: "Data-driven decisions",
+    description:
+      "Make data-driven decisions with our product data analytics. Our AI-generated reports help you unlock insights hidden in your product data.",
+  },
+  {
+    number: 3,
+    title: "Always affordable",
+    description:
+      "Always affordable pricing that scales with your business. Get top-quality product data analytics services without hidden costs or unexpected fees.",
+  },
+];
+
 export const WorkitLandingPage = () => {
-    return (
-        <div>
-        <h1>WorkitLandingPage</h1>
+  return (
+    <div>
+      <img
+        src={BackgroundPurple}
+        alt="Background"
+        style={{
+          width: "100%",
+          position: "absolute",
+          zIndex: 1,
+        }}
+      />
+      <img
+        src={BackgroundGhostWhite}
+        alt="Background"
+        style={{
+          width: "100%",
+          height: "105rem",
+          position: "absolute",
+          zIndex: 0,
+        }}
+      />
+      <div className="wrapper">
+        <div className="headerWrapper">
+          <img src={LogoLight} alt="Logo" className="header1" />
+          <div className="header2Wrapper">
+            <h1 className="header2">Apply for access</h1>
+            <div className="header2Underline"></div>
+          </div>
         </div>
-    );
-}
+        <h1
+          className="title"
+          style={{
+            fontVariationSettings: "'opsz' 144, 'wght' 600, 'SOFT' 0",
+          }}
+        >
+          Data <span>tailored</span> to your needs.
+        </h1>
+        <button className="learnMoreButton">Learn more</button>
+        <div className="iphoneWrapper">
+          <img src={Iphone} alt="Iphone" className="iphone" />
+        </div>
+        {socialProofData.map((item, index) => (
+          <SocialProof key={index} {...item} />
+        ))}
+        <img src={Founder} alt="Founder" className="founder" />
+        <div className="founderSpeech">
+          <h2
+            style={{
+              fontVariationSettings: "'opsz' 144, 'wght' 600, 'SOFT' 0",
+            }}
+          >
+            Be the first to test
+          </h2>
+          <span>
+            Hi, I'm Louis Graham, the founder of the company. Book a demo call
+            with me to become a beta tester for our app and kickstart your
+            company. Apply for access below and I’ll be in touch to schedule a
+            call.
+          </span>
+          <button className="applyButton">Apply for access</button>
+        </div>
+        <div className="footer">
+          <img src={LogoDark} alt="Logo" className="footerLogo" />
+          <div className="footerSocialNetworks">
+            <img src={FacebookLogo} alt="Facebook" />
+            <img src={TwitterLogo} alt="Twitter" />
+            <img src={InstagramLogo} alt="Instagram" />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
